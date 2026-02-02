2c305da16b01713a2adaa7b59d4cd91a39657918
Roberto Luo <r13luo@gmail.com>
2026-02-02 14:06:56 -0500

    add payment component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
 import "./App.css";
+import Payment from "./components/Payment";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div class="navbar bg-base-100 shadow-sm">
+      <div class="navbar bg-base-300 shadow-sm">
         <a class="btn btn-ghost text-xl">Reto Grupo Verde</a>
       </div>
+      <Payment />
     </>
   );
 }
